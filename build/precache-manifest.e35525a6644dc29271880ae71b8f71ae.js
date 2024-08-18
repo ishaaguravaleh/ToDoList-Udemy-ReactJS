b9e81f12c95c2bf9120de9cc4e6d2b8f5c46b826
Isha.G <isha.guravaleh@qurinomsolutions.com>
2024-08-18 16:41:20 +0530

    chnged

---
+++ build/precache-manifest.e35525a6644dc29271880ae71b8f71ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287eca1315e5cf1e6ce5b612a40be41c",
+    "url": "/ishaaguravaleh/ToDoList-Udemy-ReactJS/index.html"
+  },
+  {
+    "revision": "1e9046ac300eca03adcc",
+    "url": "/ishaaguravaleh/ToDoList-Udemy-ReactJS/static/js/2.b9fef26f.chunk.js"
+  },
+  {
+    "revision": "36eff4072c8d3336d64c",
+    "url": "/ishaaguravaleh/ToDoList-Udemy-ReactJS/static/js/main.6896dd6d.chunk.js"
+  },
+  {
+    "revision": "770e55d205fec00127eb",
+    "url": "/ishaaguravaleh/ToDoList-Udemy-ReactJS/static/js/runtime-main.2b476b7e.js"
+  }
+]);
